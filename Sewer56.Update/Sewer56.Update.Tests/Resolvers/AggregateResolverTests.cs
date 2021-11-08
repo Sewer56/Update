d8326bb5153f021a8782ac8b87cbf1dc419fe57b
Sewer 56 <seweratwork@gmail.com>
2021-11-08 16:31:30 +0000

    Added: Aggregate Package Resolver

---
+++ Sewer56.Update/Sewer56.Update.Tests/Resolvers/AggregateResolverTests.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NuGet.Versioning;
+using Sewer56.DeltaPatchGenerator.Lib.Utility;
+using Sewer56.Update.Interfaces;
+using Sewer56.Update.Packaging.Structures;
+using Sewer56.Update.Resolvers;
+using Sewer56.Update.Resolvers.GameBanana;
+using Sewer56.Update.Resolvers.GitHub;
+using Sewer56.Update.Structures;
+using Xunit;
+
+namespace Sewer56.Update.Tests.Resolvers;
+
+public class AggregateResolverTests
+{
+    public string PackageFolder = Path.Combine(Assets.TempFolder, "Package");
+    public string OutputFolder = Path.Combine(Assets.TempFolder, "Output");
+    public string MetadataFolder = Path.Combine(Assets.TempFolder, "Metadata");
+
+    public GameBananaResolverConfiguration GameBananaConfig = new GameBananaResolverConfiguration()
+    {
+        ItemId = 333681
+    };
+
+    public GitHubResolverConfiguration GitHubConfig = new GitHubResolverConfiguration()
+    {
+        UserName = "Sewer56",
+        RepositoryName = "Update.Test.Repo"
+    };
+
+    public AggregateResolverTests()
+    {
+        IOEx.TryDeleteDirectory(Assets.TempFolder);
+        Directory.CreateDirectory(PackageFolder);
+    }
+
+    [Fact]
+    public async Task GetPackageVersionsAsync_FindsVersionsOnlyInOneSource()
+    {
+        var commonResolverSettings = new CommonPackageResolverSettings() { AllowPrereleases = true };
+
+        // Act
+        var resolver = new AggregatePackageResolver(new List<IPackageResolver>()
+        {
+            new GameBananaUpdateResolver(GameBananaConfig, commonResolverSettings),
+            new GitHubReleaseResolver(GitHubConfig, commonResolverSettings),
+        });
+
+        await resolver.InitializeAsync();
+        var versions = await resolver.GetPackageVersionsAsync();
+
+        // Assert (Shared)
+        Assert.Contains(new NuGetVersion("3.0"), versions); 
+        Assert.Contains(new NuGetVersion("2.0"), versions);
+        Assert.Contains(new NuGetVersion("1.0"), versions);
+
+        // Assert (GitHub Only)
+        Assert.Contains(new NuGetVersion("3.0-pre"), versions);
+    }
+
+    [Fact]
+    public async Task DownloadPackageAsync_CanDownloadItemFromNonDefaultSource()
+    {
+        var packageFilePath = Path.Combine(PackageFolder, "Package.pkg");
+        var commonResolverSettings = new CommonPackageResolverSettings() { AllowPrereleases = true };
+
+        // Act
+        var resolver = new AggregatePackageResolver(new List<IPackageResolver>()
+        {
+            new GameBananaUpdateResolver(GameBananaConfig, commonResolverSettings),
+            new GitHubReleaseResolver(GitHubConfig, commonResolverSettings),
+        });
+
+        await resolver.InitializeAsync();
+
+        // GitHub Only Package
+        await resolver.DownloadPackageAsync(new NuGetVersion("3.0-pre"), packageFilePath, new ReleaseMetadataVerificationInfo() { FolderPath = this.OutputFolder });
+
+        // Assert
+        Assert.True(File.Exists(packageFilePath));
+    }
+
+}
---
+++ Sewer56.Update/Sewer56.Update/Resolvers/AggregatePackageResolver.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using NuGet.Versioning;
+using Sewer56.Update.Interfaces;
+using Sewer56.Update.Packaging.Structures;
+
+namespace Sewer56.Update.Resolvers;
+
+/// <summary>
+/// A package resolver that supports downloading of packages from multiple sources.
+/// </summary>
+public class AggregatePackageResolver : IPackageResolver
+{
+    private AggregatePackageResolverItem[] _resolverItems;
+    private bool _hasAcquiredPackages;
+
+    /// <summary/>
+    /// <param name="resolvers">A list of existing resolvers.</param>
+    public AggregatePackageResolver(List<IPackageResolver> resolvers)
+    {
+        _resolverItems = new AggregatePackageResolverItem[resolvers.Count];
+        for (int x = 0; x < _resolverItems.Length; x++)
+            _resolverItems[x].Resolver = resolvers[x];
+    }
+
+    /// <inheritdoc />
+    public async Task InitializeAsync()
+    {
+        foreach (var resolver in _resolverItems)
+            await resolver.Resolver.InitializeAsync();
+    }
+
+    /// <inheritdoc />
+    public async Task<List<NuGetVersion>> GetPackageVersionsAsync(CancellationToken cancellationToken = default)
+    {
+        await AcquirePackagesIfNecessaryAsync(cancellationToken);
+        return FlattenVersions();
+    }
+
+    /// <inheritdoc />
+    public async Task DownloadPackageAsync(NuGetVersion version, string destFilePath, ReleaseMetadataVerificationInfo verificationInfo, IProgress<double>? progress = null, CancellationToken cancellationToken = default)
+    {
+        await AcquirePackagesIfNecessaryAsync(cancellationToken);
+        var resolver = GetResolverForVersion(version);
+        await resolver.DownloadPackageAsync(version, destFilePath, verificationInfo, progress, cancellationToken);
+    }
+
+    private List<NuGetVersion> FlattenVersions()
+    {
+        var hashSet = new HashSet<NuGetVersion>();
+        foreach (var resolver in _resolverItems)
+        foreach (var version in resolver.Versions)
+            hashSet.Add(version);
+
+        var result = hashSet.ToList();
+        result.Sort((a, b) => a.CompareTo(b));
+        return result;
+    }
+
+    private IPackageResolver GetResolverForVersion(NuGetVersion version)
+    {
+        foreach (var resolver in _resolverItems)
+        {
+            if (resolver.Versions.Find(x => x.Equals(version)) != default)
+                return resolver.Resolver;
+        }
+
+        throw new Exception("Resolver with a specified version was not found.");
+    }
+
+    private async Task AcquirePackagesIfNecessaryAsync(CancellationToken token)
+    {
+        if (_hasAcquiredPackages)
+            return;
+
+        for (var x = 0; x < _resolverItems.Length; x++)
+            _resolverItems[x].Versions = await _resolverItems[x].Resolver.GetPackageVersionsAsync(token);
+
+        _hasAcquiredPackages = true;
+    }
+
+    private struct AggregatePackageResolverItem
+    {
+        public IPackageResolver Resolver = default!;
+        public List<NuGetVersion> Versions = default!;
+    }
+}
