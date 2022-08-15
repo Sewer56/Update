aa183f95aafd5a8c5b77a80f96b41e0bf099a9f2
Sewer 56 <seweratwork@gmail.com>
2022-08-15 02:20:08 +0100

    Added: Stream Extensions Overload for known Content Length

--- Sewer56.Update/Sewer56.Update.Misc/StreamExtensions.cs
+++ Sewer56.Update/Sewer56.Update.Misc/StreamExtensions.cs
@@ -26,9 +26,54 @@ public static class StreamExtensions
         await destination.WriteAsync(buffer, 0, bytesCopied, cancellationToken);
         return bytesCopied;
     }
-    
+
     /// <summary>
     /// Copies data from a stream to another stream asynchronously, with support for 
+    /// </summary>
+    /// <param name="source">Where to copy the data from.</param>
+    /// <param name="destination">Where to copy the data to.</param>
+    /// <param name="bufferSize">Size of chunks used to copy from source to destination.</param>
+    /// <param name="progress">Can be used to report current copying progress.</param>
+    /// <param name="contentLength">Length of content to be downloaded. Provide this if source stream doesn't support length property but it is known.</param>
+    /// <param name="cancellationToken">Can be used to cancel the operation.</param>
+    public static async Task CopyToAsyncEx(this Stream source, Stream destination, int bufferSize = 262144, IProgress<double>? progress = null, long? contentLength = null, CancellationToken cancellationToken = default)
+    {
+        using var buffer = new ArrayRental<byte>(bufferSize);
+        var totalBytesCopied = 0L;
+        int bytesCopied;
+
+        bool supportsLength = true;
+        long length = 0;
+
+        if (!contentLength.HasValue)
+        {
+            try
+            {
+                length = source.Length;
+                if (length == 0)
+                    length = 1; // just in case.
+            }
+            catch (Exception e) { supportsLength = false; }
+        }
+        else
+        {
+            length = contentLength.Value;
+        }
+
+        do
+        {
+            bytesCopied = await source.CopyBufferedToAsync(destination, buffer.Array, cancellationToken);
+            totalBytesCopied += bytesCopied;
+            if (supportsLength)
+                progress?.Report((double)totalBytesCopied / length);
+        }
+        while (bytesCopied > 0);
+
+        progress?.Report(1.0);
+    }
+
+    /// <summary>
+    /// Copies data from a stream to another stream asynchronously, with support for progress reporting.
     /// </summary>
     /// <param name="source">Where to copy the data from.</param>
     /// <param name="destination">Where to copy the data to.</param>
@@ -37,23 +82,6 @@ public static class StreamExtensions
     /// <param name="cancellationToken">Can be used to cancel the operation.</param>
     public static async Task CopyToAsyncEx(this Stream source, Stream destination, int bufferSize = 262144, IProgress<double>? progress = null, CancellationToken cancellationToken = default)
     {
-        using var buffer = new ArrayRental<byte>(bufferSize);
-        var totalBytesCopied = 0L;
-        int bytesCopied;
-        bool supportsLength = true;
-
-        try { var _ = source.Length; }
-        catch (Exception e) { supportsLength = false; }
-
-        do
-        {
-            bytesCopied = await source.CopyBufferedToAsync(destination, buffer.Array, cancellationToken);
-            totalBytesCopied += bytesCopied;
-            if (supportsLength)
-                progress?.Report((double) totalBytesCopied / source.Length);
-        } 
-        while (bytesCopied > 0);
-
-        progress?.Report(1.0);
+        await CopyToAsyncEx(source, destination, bufferSize, progress, null, cancellationToken);
     }
 }
